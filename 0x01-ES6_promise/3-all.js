3333ee76c98d3b63839065c98f2927da5fc998ea
yasoug <ougouisyassine@gmail.com>
2024-01-03 21:50:16 +0000

    task 3 : commit 1

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then(([{ body }, { firstName, lastName }]) => {
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
