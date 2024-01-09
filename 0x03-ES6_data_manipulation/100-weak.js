dcbb151aa959d9afccae255c25c54e29d890dc8b
yasoug <ougouisyassine@gmail.com>
2024-01-09 19:38:37 +0000

    task 11 : commit 1

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,9 @@
+export const weakMap = new WeakMap();
+export function queryAPI(endpoint) {
+  let count = weakMap.get(endpoint) || 0;
+  count += 1;
+  weakMap.set(endpoint, count);
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
