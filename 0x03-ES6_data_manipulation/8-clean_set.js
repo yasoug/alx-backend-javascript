ccb2d4964c654b068eb03510ebc3034de8f507be
yasoug <ougouisyassine@gmail.com>
2024-01-09 19:22:31 +0000

    task 8 : commit 1

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') return '';
+  return Array.from(set)
+    .filter((value) => value && value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+}
