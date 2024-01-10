ba8315ccc7704d07fdc1afb1a18e38a0b98daf45
yasoug <ougouisyassine@gmail.com>
2024-01-10 20:51:39 +0000

    task 8. Ambient Namespaces

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,16 @@
+/// <reference path='./crud.d.ts' />
+
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+
+let row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+const updateRow: RowElement = { ...row, age: 23 };
+
+CRUD.updateRow(newRowID, updateRow);
+
+CRUD.deleteRow(newRowID);
