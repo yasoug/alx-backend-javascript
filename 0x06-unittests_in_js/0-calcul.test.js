d46f8cd50611feba7afc45f7480ff966773df243
yasoug <ougouisyassine@gmail.com>
2024-02-29 22:05:01 +0000

    task 0 : commit x (plz)

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,13 +1,16 @@
-const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const assert = require("assert");
+const calculateNumber = require("./0-calcul.js");
 
 describe('calculateNumber', () => {
-  it('rounding a', () => {
-    assert.equal(calculateNumber(1.4, 1), 2);
-    assert.equal(calculateNumber(2.6, 2), 5);
+  it('round the first argument', () => {
+    assert.equal(calculateNumber(1.3, 0), 1);
   });
 
-  it('rounding b', () => {
-    assert.equal(calculateNumber(0, 1.4), 1);
+  it('round the second argument', () => {
     assert.equal(calculateNumber(1, 1.7), 3);
   });
+
+  it('round both arguments', () => {
+    assert.equal(calculateNumber(1.3, 1.7), 3);
+  });
+});
